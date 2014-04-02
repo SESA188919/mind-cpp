07108fd65d2454ed6d6f0c7b2e3252249da10cac
Yves Teissier <yves.teissier@non.schneider-electric.com>
2014-04-02 11:02:32 +0200

    Remove comments

--- mindcpp/src/assemble/resources/examples/ZigBeeSensor/src/main/mind/FlashMng.cpp
+++ mindcpp/src/assemble/resources/examples/ZigBeeSensor/src/main/mind/FlashMng.cpp
@@ -5,14 +5,12 @@
 void flashmngitf::WritePage(int address, int byte) {
   FlashMngExt flash;
   flash.WriteByte(address, byte);
-  //PRIVATE.toto = 2;
-  //PRIVATE.tata = 4;
   return;
 }
 
 
 void flashmngitf::ReadPage(int address, int *byte) {
-  //	PRIVATE.m_FlashMngExt.ReadByte(address, byte);
-  *byte = 1;
+  FlashMngExt flash;
+  flash.ReadByte(address, byte);
   return;
 }
